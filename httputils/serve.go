8c8c669a080421375813f2afe7173f94dd4f80e8
k.nitta <mmchari.0228@gmail.com>
2019-06-18 21:20:58 +0900

    served echo

---
+++ httputils/serve.go
@@ -0,0 +1,40 @@
+package httputils
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+// Serve ...
+func Serve(h http.Handler) {
+
+	srv := http.Server{
+		Addr:    ":8080",
+		Handler: h,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, os.Interrupt)
+	sig := <-quit // wait Signal
+	fmt.Printf("SIGNAL %+v recieved, then sutting down...\n", sig)
+
+	// Shutdown server
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		fmt.Fprintln(os.Stderr, "failed to gracefully shutdown:", err)
+	} else {
+		fmt.Println("succeed to gracefully shutdown")
+	}
+}
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/kazukousen/go-api-utils/httputils"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Get("/echo", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("hello"))
+	})
+
+	httputils.Serve(r)
+}
